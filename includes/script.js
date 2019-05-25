edaa8db25239c3a9d26f2a0a137b5db09041a076
shani24levi <shanilevi01011@gmail.com>
2019-05-26 02:04:15 +0300

    changes3

--- includes/script.js
+++ includes/script.js
@@ -1,22 +1,10 @@
-jQuery(document).ready(function($) {
- 
-    $('#myCarousel').carousel({
-            interval: 5000
-    });
 
-    $('#carousel-text').html($('#slide-content-0').html());
+/* for butoons on click */
+function pageLayout2() {
+    location.replace('layout2.html');
+}
 
-    //Handles the carousel thumbnails
-   $('[id^=carousel-selector-]').click( function(){
-        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
-        var id = parseInt(id);
-        $('#myCarousel').carousel(id);
-    });
+function pageLayout3() {
+    location.replace('layout3.html');
+  }
 
-
-    // When the carousel slides, auto update the text
-    $('#myCarousel').on('slid.bs.carousel', function (e) {
-             var id = $('.item.active').data('slide-number');
-            $('#carousel-text').html($('#slide-content-'+id).html());
-    });
-});
