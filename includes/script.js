986a616eda50f8afa588ce091e8d40d59ff49dd0
shani24levi <shanilevi01011@gmail.com>
2019-05-27 05:23:51 +0300

    chang-fo-now

--- includes/script.js
+++ includes/script.js
@@ -1,10 +1,53 @@
 
 /* for butoons on click */
 function pageLayout2() {
-    location.replace('layout2.html');
+    location.replace('listDogsPage.html');
 }
 
 function pageLayout3() {
-    location.replace('layout3.html');
-  }
+    location.replace('AddDogPage.html');
+}
+
+function pageIndex() {
+    location.replace('index.html');
+}
+
+function myDogINFO() {
+    location.replace('myDoginfoPage.html');
+}
+
+function goBackTOindex() {
+    location.replace('index.html');
+}
+
+function goBackTOlist() {
+    location.replace('listDogsPage.html');
+}
+
+
+
+/*for galary - page 'myDoginfoPage.html' */
+jQuery(document).ready(function($) {
+ 
+    $('#myCarousel').carousel({
+            interval: 5000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+   $('[id^=carousel-selector-]').click( function(){
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+             var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-'+id).html());
+    });
+});
+/*----------------------------------------------------------------*/
 
